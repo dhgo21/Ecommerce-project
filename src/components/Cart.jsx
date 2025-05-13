2711740684ae24fb5533e9f7906253deb4a23993
dhgo21 <goyal.dhruv2106@gmail.com>
2025-05-14 03:44:27 +0530

    commit

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,7 @@
 import "./Cart.css";
 import { Link } from 'react-router-dom';
 import { IoMdClose } from "react-icons/io";
-
+import { IoCart } from "react-icons/io5";
 function Cart({ cart, setcart }) {
 
 
@@ -30,10 +30,13 @@ function Cart({ cart, setcart }) {
     }
   }
 
-  const total = cart.reduce((price, item) => {
-    const numericPrice = Number(item.price.replace(/[^0-9]/g, ""));
-    return price + item.qty * numericPrice;
-  }, 0);
+const total = cart.reduce((price, item) => {
+  const numericPrice = Number(item.price.replace(/[^0-9]/g, ""));
+  return price + item.qty * numericPrice;
+}, 0);
+
+const formattedTotal = total.toLocaleString("en-IN");
+
 
   return (
     <>
@@ -41,47 +44,79 @@ function Cart({ cart, setcart }) {
         <h3>Cart</h3>
         {cart.length === 0 && (
           <div className="emptycart">
+            <IoCart className="emptycart"/>
             <h2>Your Shopping Cart is Empty</h2>
             <Link to="/shop"><button>Shop Now</button></Link>
           </div>
         )}
         <div className="container">
+          <div className="leftbox">
           {cart.map((curr) => {
             const numericPrice = Number(curr.price.replace(/[^0-9]/g, ""));
+            const totalPrice = numericPrice * curr.qty;
+            const formattedPrice = totalPrice.toLocaleString("en-IN");
             return (
-              <div className="box" key={curr.id}>
-                <div className="imgbox">
-                  <img src={curr.image} alt={curr.name}></img>
+              <>
+                <div className="box" key={curr.id}>
+                  <div className="imgbox">
+                    <img src={curr.image} alt={curr.name}></img>
+                  </div>
+                  <div className="detail">
+                    <div className="info">
+                      <h4>{curr.cat}</h4>
+                      <h3>{curr.name}</h3>
+                      <p>Price ({curr.qty} Quantity): {formattedPrice}</p>
+                    </div>
+                    <div className="quantity">
+                      <button onClick={() => decqty(curr)}>-</button>
+                      <input type="number" value={curr.qty} readOnly />
+                      <button onClick={() => inqty(curr)}>+</button>
+                    </div>
+                    <div className="icon">
+                      <li onClick={() => removeproduct(curr)}><IoMdClose /></li>
+                    </div>
+                  </div>
                 </div>
-                <div className="detail">
-                  <div className="info">
-                    <h4>{curr.cat}</h4>
-                    <h3>{curr.name}</h3>
-                    <p>Price: {curr.price}</p>
-                    <p>Total: ₹{numericPrice * curr.qty}</p>
-                  </div>
-                  <div className="quantity">
-                    <button onClick={() => inqty(curr)}>+</button>
-                    <input type="number" value={curr.qty} readOnly />
-                    <button onClick={() => decqty(curr)}>-</button>
-                  </div>
-                  <div className="icon">
-                    <li onClick={() => removeproduct(curr)}><IoMdClose /></li>
-                  </div>
-                </div>
-              </div>
+              </>
             );
           })}
-        </div>
-        <div className="bottom">
-          {cart.length > 0 && (
-            <div className="bottomwork">
-              <div className="total">
-                <h4>Sub Total: ₹{total}</h4>
+          </div>
+          {
+            cart.length>0 && (
+              <div className="rightbox">
+                <div className="rightboxheading">
+                  <span>PRICE DETAILS</span>
+                </div>
+                <div className="pricedetails">
+                  <table>
+                    <tbody>
+                      <tr>
+                        <td>Price ({cart.length} item)</td>
+                        <td>₹{formattedTotal}</td>
+                      </tr>
+                      <tr>
+                        <td>Packaging Fee</td>
+                        <td>₹29</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                  <div className="totalprice">
+                    <table>
+                      <tbody>
+                        <tr>
+                          <td>Total Amount</td>
+                          <td>₹{(total + 29).toLocaleString("en-IN")}</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                  <div className="checkout">
+                  <button>Checkout</button>
+                </div>
+                </div>
               </div>
-              <button>Checkout</button>
-            </div>
-          )}
+            )
+          }
         </div>
       </div>
     </>
